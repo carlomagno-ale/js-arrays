19b6759014ce0645f12cf9e44cd351c21e95856d
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-12 17:16:59 +0100

    add es3 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,9 +15,13 @@ console.log(teachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
+
 const longNames = null;
 
+
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(1, 1,);
+console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
