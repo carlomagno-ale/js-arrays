35a75d542beaad2e8f816f53c5abf687e6f9589d
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-12 16:30:42 +0100

    edit es 3 and 4

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -18,16 +18,17 @@ teachers.splice(4, 1, 'Patrick');
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = teachers[6];
-teachers.pop()
-console.log(teachers);
+const lastTeacher = teachers.pop();
 console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = teachers[0];
+const firstTeacher = teachers.shift();
+console.log(firstTeacher);
+
+/*const firstTeacher = teachers[0];
 teachers.shift()
 console.log(teachers);
-console.log(firstTeacher);
+console.log(firstTeacher);*/
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push('Vanessa');
