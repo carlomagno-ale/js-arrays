3fd1b673eed5086fcdd9539c975da100bdb8cb35
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-12 19:40:40 +0100

    add es4 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,8 +15,15 @@ console.log(teachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
 
+for ( let i = 0 ; i < teachers.length ; i++) {
+  if ( teachers[i].length >= 5 ){
+     longNames.push(teachers[i]);
+  }
+}
+
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 teachers.splice(1, 1,);
@@ -24,8 +31,13 @@ console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
+const isFabioPresent = teachers.includes('Fabio');
 
-const isFabioPresent = null;
+if (isFabioPresent) {
+  console.log("Fabio è presente!")
+} else {
+  console.log("Fabio è in ritardo!")
+}
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
