be9946270cf479e60375621bd2ccdb607a8547a7
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-12 18:20:52 +0100

    edit es8

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -45,9 +45,10 @@ console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = teachers.length;
-if (isTeachersEmpty === 0) { 
-  console.log("l'array è vuoto!") 
+
+const isTeachersEmpty = teachers.length === 0;
+if (isTeachersEmpty) { 
+  console.log("l'array è vuoto!")
 } else {
-  console.log(`l'array è di ${isTeachersEmpty}`)
+  console.log(`l'array è pieno`)
 }
--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,7 +15,6 @@ console.log(teachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-
 const longNames = null;
 
 
@@ -25,6 +24,7 @@ console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
+
 const isFabioPresent = null;
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
